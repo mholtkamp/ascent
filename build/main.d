f1bec3ef3bef5ed3623fe9ead7da036e2cf1fe92
mholtkamp <mholt012@gmail.com>
2015-12-17 16:25:34 -0500

    Removing files that will now be ignored

--- build/main.d
+++
@@ -1,9 +0,0 @@
-main.o: e:/projects/ascent/source/main.c \
- e:/projects/ascent/headers/util.h e:/projects/ascent/headers/registers.h \
- e:/projects/ascent/headers/constants.h
-
-e:/projects/ascent/headers/util.h:
-
-e:/projects/ascent/headers/registers.h:
-
-e:/projects/ascent/headers/constants.h:
--- build/util.d
+++
@@ -1,9 +0,0 @@
-util.o: e:/projects/ascent/source/util.c \
- e:/projects/ascent/headers/util.h e:/projects/ascent/headers/registers.h \
- e:/projects/ascent/headers/constants.h
-
-e:/projects/ascent/headers/util.h:
-
-e:/projects/ascent/headers/registers.h:
-
-e:/projects/ascent/headers/constants.h:
